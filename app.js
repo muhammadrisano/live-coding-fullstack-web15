1384409c2c51f80374bf75818230e93dcc88f6fb
eno <muhammadrisano@gmail.com>
2020-06-10 09:10:20 +0700

    install local

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 const bodyParser = require('body-parser')
 const route = require('./src/routers/index.js');
 const cors = require('cors');
